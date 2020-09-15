8f9466cd85cf8473333f75481447dd336b91c242
snake-whisper <snake-whisper@web-utils.eu>
2020-09-15 20:11:26 +0000

    add package structure

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="puck-dns-api-SNAKE-WHISPER", # Replace with your own username
+    version="0.0.1",
+    author="Snake-Whisper",
+    author_email="snake-whisper@web-utils.eu",
+    description="Python API for the great free DNS Service \"PUCK\" from Daniel J. Luke (http://puck.nether.net/dns)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Snake-Whisper/puck-dns-api",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.1',
+)
